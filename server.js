0177f2cde35b9aca09ebd183c91a94b72fcbf6f8
youssefali123 <77068196+youssefali123@users.noreply.github.com>
2025-05-06 18:55:51 +0300

    Add files via upload

--- server.js
+++ server.js
@@ -14,33 +14,33 @@ app.use(bodyParser.json());
 
 connectDB().then(async () => {
   // Scrape some pages (can be static for now)
-  // await crawl('https://yalla--shoot.today/');
-  // await crawl('https://wikipedia.org');
-  // await crawl('https://www.filgoal.com/');
-  // await crawl('https://wecima.film/');
-  // await crawl('https://www.mozilla.org/');
-  // await crawl('https://www.w3schools.com/');
-  // await crawl('https://www.google.com/');
-  // await crawl('https://www.facebook.com/');
-  // await crawl('https://www.instagram.com/');
-  // await crawl('https://twitter.com/');
-  // await crawl('https://www.linkedin.com/');
-  // await crawl('https://www.youtube.com/');
-  // await crawl('https://www.tiktok.com/');
-  // await crawl('https://www.snapchat.com/');
-  // await crawl('https://www.reddit.com/');
-  // await crawl('https://www.pinterest.com/');
-  // await crawl('https://www.quora.com/');
-  // await crawl("https://www.youm7.com/");
-  // await crawl("https://www.aljazeera.net/");
-  // await crawl("https://www.almotaher.com/");
-  // await crawl("https://www.imdb.com/")
-  // await crawl("https://www.bbc.com/");
-  // await crawl("https://www.cnn.com/");
-  // await crawl("https://www.nbcnews.com/");
-  // await crawl("https://www.msn.com/");
-  // await crawl("https://www.foxnews.com/");
-  // await crawl("https://www.cbsnews.com/");
+  await crawl('https://yalla--shoot.today/');
+  await crawl('https://wikipedia.org');
+  await crawl('https://www.filgoal.com/');
+  await crawl('https://wecima.film/');
+  await crawl('https://www.mozilla.org/');
+  await crawl('https://www.w3schools.com/');
+  await crawl('https://www.google.com/');
+  await crawl('https://www.facebook.com/');
+  await crawl('https://www.instagram.com/');
+  await crawl('https://twitter.com/');
+  await crawl('https://www.linkedin.com/');
+  await crawl('https://www.youtube.com/');
+  await crawl('https://www.tiktok.com/');
+  await crawl('https://www.snapchat.com/');
+  await crawl('https://www.reddit.com/');
+  await crawl('https://www.pinterest.com/');
+  await crawl('https://www.quora.com/');
+  await crawl("https://www.youm7.com/");
+  await crawl("https://www.aljazeera.net/");
+  await crawl("https://www.almotaher.com/");
+  await crawl("https://www.imdb.com/")
+  await crawl("https://www.bbc.com/");
+  await crawl("https://www.cnn.com/");
+  await crawl("https://www.nbcnews.com/");
+  await crawl("https://www.msn.com/");
+  await crawl("https://www.foxnews.com/");
+  await crawl("https://www.cbsnews.com/");
 
   // Build the index
   await buildIndex();
